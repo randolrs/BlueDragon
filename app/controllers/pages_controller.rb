fa48a09697042bfd1a9c89ca5c465813e0ca85d1
Randolrs <R.Shane.Randolph@gmail.com>
2015-06-15 12:34:19 -0500

    Changed article rendering on front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,11 @@ class PagesController < ApplicationController
 
   	@leadarticle = Article.find_by_lead(true)
  
-  	@articles1 = Article.all.order("created_at DESC").limit(12)
-  	@articles2 = Article.all.order("created_at DESC").limit(12).offset(12)
+  	@articles1 = Article.where("category = :business or :politics or :technology or :academia", {business: "Business", politics:"Politics", technology: "Technology", academia: "Academia"}).order("created_at DESC").limit(12)
+  	@articles2 = Article.where("category = :sports or :entertainment or :music or :art", {sports: "Sports", music:"Music", entertainment: "Entertainment", art: "Art"}).order("created_at DESC").limit(12)
+    @articles3 = Article.where("category = :literature or :fashion or :leisure", {literature: "Literature", fashion:"Fashion & Style", leisure: "Leisure",}).order("created_at DESC").limit(12)
+
+    #@articles2 = Article.all.order("created_at DESC").limit(12).offset(12)
   	@articles3 = Article.all.order("created_at DESC").limit(12).offset(24)
 
   	@columnists1 = Columnist.all.order("created_at DESC").limit(5).order("rating DESC")
