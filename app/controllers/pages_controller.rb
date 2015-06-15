cbae920ff8107b368848878fdbed2c73d9fe6e57
Randolrs <R.Shane.Randolph@gmail.com>
2015-06-15 12:43:29 -0500

    reset query syntax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,9 @@ class PagesController < ApplicationController
 
   	@leadarticle = Article.find_by_lead(true)
  
-  	@articles1 = Article.where("category = :business or :politics or :technology or :academia", {business: "Business", politics:"Politics", technology: "Technology", academia: "Academia"}).order("created_at DESC").limit(12)
-  	@articles2 = Article.where("category = :sports or :entertainment or :music or :art", {sports: "Sports", music:"Music", entertainment: "Entertainment", art: "Art"}).order("created_at DESC").limit(12)
-    @articles3 = Article.where("category = :literature or :fashion or :leisure", {literature: "Literature", fashion:"Fashion & Style", leisure: "Leisure",}).order("created_at DESC").limit(12)
+  	@articles1 = Article.where("category = :business or category = :politics or category = :technology or category = :academia", {business: "Business", politics:"Politics", technology: "Technology", academia: "Academia"}).order("created_at DESC").limit(12)
+  	@articles2 = Article.where("category = :sports or category = :entertainment or category = :music or category = :art", {sports: "Sports", music:"Music", entertainment: "Entertainment", art: "Art"}).order("created_at DESC").limit(12)
+    @articles3 = Article.where("category = :literature or category = :fashion or category = :leisure", {literature: "Literature", fashion:"Fashion & Style", leisure: "Leisure",}).order("created_at DESC").limit(12)
 
     #@articles2 = Article.all.order("created_at DESC").limit(12).offset(12)
   	@articles3 = Article.all.order("created_at DESC").limit(12).offset(24)
