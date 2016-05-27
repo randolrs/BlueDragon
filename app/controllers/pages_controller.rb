75094f9a235c931a73b0da9e78dd9facb1702405
randolrs <r.shane.randolph@gmail.com>
2016-05-27 17:42:28 -0500

    all along

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,9 @@ class PagesController < ApplicationController
 
   	@leadarticle = Article.find_by_lead(true)
  
-  	@articles1 = Article.where("category_1_id = :business or category_1_id = :politics", {business: 1, politics: 4}).order("created_at DESC").limit(12)
-  	@articles2 = Article.where("category_1_id = :sports or category_1_id = :entertainment", {sports: 3, entertainment: 2}).order("created_at DESC").limit(12)
-    @articles3 = Article.where("category_1_id = :world or category_1_id = :culture", {world: 5, culture: 6}).order("created_at DESC").limit(12)
+  	@articles1 = Article.where("category_1_id = :business or category_1_id = :politics and not id = :lead_article_id", {business: 1, politics: 4, lead_article_id: @leadarticle.id}).order("created_at DESC").limit(12)
+  	@articles2 = Article.where("category_1_id = :sports or category_1_id = :entertainment and not id = :lead_article_id", {sports: 3, entertainment: 2, lead_article_id: @leadarticle.id}).order("created_at DESC").limit(12)
+    @articles3 = Article.where("category_1_id = :world or category_1_id = :culture and not id = :lead_article_id", {world: 5, culture: 6, lead_article_id: @leadarticle.id}).order("created_at DESC").limit(12)
 
   	# @columnists1 = Columnist.all.order("created_at DESC").limit(5).order("rating DESC")
   	# @columnists2 = Columnist.all.order("created_at DESC").limit(5).offset(5).order("rating DESC")
@@ -25,20 +25,12 @@ class PagesController < ApplicationController
 
     @category = Category.find_by_url(params[:category])
 
-    @leadarticle = Article.find_by_lead(true)
+    @leadarticle = Article.find(@category.top_article_id)
  
-    @articles1 = Article.where("category = :business or category = :politics or category = :technology or category = :academia", {business: "Business", politics:"Politics", technology: "Technology", academia: "Academia"}).order("created_at DESC").limit(12)
-    @articles2 = Article.where("category = :sports or category = :entertainment or category = :music or category = :art", {sports: "Sports", music:"Music", entertainment: "Entertainment", art: "Art & Design"}).order("created_at DESC").limit(12)
-    @articles3 = Article.where("category = :literature or category = :fashion or category = :leisure", {literature: "Literature", fashion:"Fashion & Style", leisure: "Leisure",}).order("created_at DESC").limit(12)
-
-    #@articles2 = Article.all.order("created_at DESC").limit(12).offset(12)
-    @articles3 = Article.all.order("created_at DESC").limit(12).offset(24)
-
-    @columnists1 = Columnist.all.order("created_at DESC").limit(5).order("rating DESC")
-    @columnists2 = Columnist.all.order("created_at DESC").limit(5).offset(5).order("rating DESC")
-    @columinsts3 = Columnist.all.order("created_at DESC").limit(5).offset(10).order("rating DESC")
-
+    @articles = Article.where("category_1_id = :category_id and not id = :lead_article_id", {category_id: @category.id, lead_article_id: @leadarticle.id}).order("created_at DESC").limit(12)
+    
     @all_category = Category.find_by_name("All")
+
     @header_categories = Category.where("parent_category_id = :all_category_id", {all_category_id: @all_category.id})
 
 
